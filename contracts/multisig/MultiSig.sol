1124e1d21ed234866e04917b9086a42e5aef52d6
elbok <kromsten@pm.me>
2022-12-05 16:12:47 +0100

    remove test contract

--- contracts/multisig/MultiSig.sol
+++ contracts/multisig/MultiSig.sol
@@ -179,20 +179,3 @@ contract MultiSigWallet {
         );
     }
 }
-
-Here is a contract to test sending transactions from the multi-sig wallet
-
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
-
-contract TestContract {
-    uint public i;
-
-    function callMe(uint j) public {
-        i += j;
-    }
-
-    function getData() public pure returns (bytes memory) {
-        return abi.encodeWithSignature("callMe(uint256)", 123);
-    }
-}
